d29997b9d54b78833fe550ac0154a6bd5a3676ed
Satoshi1123 <sato8125@gmail.com>
2020-04-22 16:08:05 +0900

    Add Code3.5

--- Chapter3/Code3.5.py
+++ Chapter3/Code3.5.py
@@ -1,3 +1,13 @@
 #Introduction to Computation and Programming Using Python
 #ニュートン-ラフソン法の実装(P36-P37)
 #Newton-Raphson method implementation(P36-P37)
+
+#平方根を求めるニュートン-ラフソン法
+#x**2 - 24 = 0で誤差がepsilon以下になるxを求める
+
+epsilon = 0.01
+k = 24.0
+guess = k/2.0
+while abs(guess*guess - k) >= epsilon:
+    guess = guess - (((guess**2) - k)/(2*guess))
+print('Square root of', k, 'is about', guess)
